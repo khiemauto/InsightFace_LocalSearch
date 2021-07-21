6f0e3bd2ef543c2e702f537e8717034be3c9b415
khiemtv <khiemtv@lienviettech.com.vn>
2021-07-21 15:30:56 +0700

    update

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-# source `which virtualenvwrapper.sh`
 conda activate object_tracking_v2
+SCRIPT_DIR=$( dirname "${BASH_SOURCE[0]}" )
+echo $( dirname "${BASH_SOURCE[0]}" )
+cd $SCRIPT_DIR
 python main.py
 conda deactivate
